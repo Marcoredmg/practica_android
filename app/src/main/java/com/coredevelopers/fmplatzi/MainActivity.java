208ed9b7e74f3ceea468f42c4fae854795be52f7
Marcore <marcore.dmg@gmail.com>
2016-06-15 00:55:11 -0500

    fragmento de boton

--- app/src/main/java/com/coredevelopers/fmplatzi/MainActivity.java
+++ app/src/main/java/com/coredevelopers/fmplatzi/MainActivity.java
@@ -3,11 +3,15 @@ package com.coredevelopers.fmplatzi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.coredevelopers.fmplatzi.ui.HypedArtistsFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main_container, new HypedArtistsFragment()).commit();
     }
 }
--- app/src/main/java/com/coredevelopers/fmplatzi/ui/HypedArtistsFragment.java
+++ app/src/main/java/com/coredevelopers/fmplatzi/ui/HypedArtistsFragment.java
@@ -1,19 +1,27 @@
 package com.coredevelopers.fmplatzi.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.coredevelopers.fmplatzi.MainActivity;
+import com.coredevelopers.fmplatzi.R;
 
 /**
  * Created by sears on 14/06/16.
  */
 public class HypedArtistsFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View root = inflater.inflate(R.layout.fragment_hyped_artists, container, false);
+        return root;
     }
+
 }
